a332fd139bf862e53d7b61ebe34618d271fc37ca
Dusu Timothy <dususunday@gmail.com>
2026-07-22 11:19:38 +0100

    Update Q3

--- test3.js
+++ test3.js
@@ -1,18 +1,23 @@
 const controller = new AbortController();
-const {signal} = controller;
+const { signal } = controller;
 
 async function fetchData() {
- try{
-  const data = await fetch('https://jsonplaceholder.typicode.com')
-  const jsonData = await Response.json
-  console.log(jsonData);
- }  
- catch (error){
- if (error.name === "AbortError")
-  console.log("");
-  
+  try {
+    const data = await fetch('https://jsonplaceholder.typicode.com')
+    const jsonData = await Response.json
+    console.log(jsonData);
+  }
+  catch (error) {
+    if (error.name === "AbortError") {
+      console.log("Successful");
 
- }
+
+    }
+   else {
+    console.log("unsuccessful");
+
+  }
+}
 }
 fetchData()
-Controller.abort()
+controller.abort()
