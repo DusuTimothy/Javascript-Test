bf354a83368593e99aa3424b28e179dd817cdafc
Dusu Timothy <dususunday@gmail.com>
2026-07-31 10:56:22 +0100

    add assessment 4 answers Question 1

--- assessment4.js
+++ assessment4.js
@@ -1,4 +1,21 @@
 //Question 1.
 const users = [
-  {name: "Timothy"}
-]
+  { name: "Timothy", age: 31 },
+  { name: "Janet", age: 17 },
+  { name: "Luke", age: 70 },
+
+];
+
+let found = users.map((index) => {
+  if (index.age < 18) {
+    console.log("You are a minor");
+  }
+  else if (index.age > 18 && index.age < 70) {
+    console.log("You are an adult");
+
+  }
+  else {
+    console.log("You are a senior");
+    
+  }
+});
